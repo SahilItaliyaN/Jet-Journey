b6c90192daa1cd04d38060f72c35379f5706444a
SahilItaliyaN <152978347+SahilItaliyaN@users.noreply.github.com>
2024-08-05 17:44:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,40 @@ app.get('/products',async (req,res)=>{
 app.delete("/product/:id",async (req,res)=>{
     const result = await Product.deleteOne({_id:req.params.id})
     res.send(result)
+});
+
+
+// For get products details
+app.get("/product/:id",async (req,res)=>{
+    let result = await Product.findOne({_id:req.params.id})
+    if(result){
+        res.send(result)
+    }else{
+        res.send({result:"Data Not Found"})
+    }
 })
+
+// For update products
+app.put("/product/:id", async (req,res)=>{
+    let result = await Product.updateOne(
+        { _id : req.params.id },
+        {
+            $set: req.body
+        }
+    )
+    res.send(result);
+})
+
+app.get("/search/:key", async(req,res)=>{
+    let result = await Product.find({
+        "$or":[
+            {name:{$regex:req.params.key}},
+            {category:{$regex:req.params.key}},
+            {company:{$regex:req.params.key}}
+        ]
+    })
+    res.send(result);
+})
+
 
 app.listen(5000)
